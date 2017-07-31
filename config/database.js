7a623028340084131def5e2783e87cd91b3c8de2
Manish Vyas <manishvyas1911@gmail.com>
2017-07-31 14:07:31 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -8,7 +8,6 @@ getPool: function () {
      { return pool;}
     pool = mysql.createPool({
   	connectionLimit :100,
-   // host     : '85.214.17.140',
       host     : 'localhost',
       user     : 'root',
       password : '',
