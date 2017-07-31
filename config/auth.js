0d2fdbcff69a089a94da22452f5c91653a984607
Manish Vyas <manishvyas1911@gmail.com>
2017-07-31 14:06:52 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,9 +4,9 @@
 module.exports = {
 
 	'facebookAuth' : {
-		'clientID' 		: '832845416864483', // your App ID
-		'clientSecret' 	: '4c5ea90c1c682cb746a8a75a1522c260', // your App Secret
-		'callbackURL' 	: 'http://localhost:3000/tms/auth/facebook/callback'
+		'clientID' 		: '*********************', // your App ID
+		'clientSecret' 	: '**************************', // your App Secret
+		'callbackURL' 	: '***************************'
 	},
 
 	'twitterAuth' : {
